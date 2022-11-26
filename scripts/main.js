fafcf7610cea3afdb1060e462bd99896254aa5c9
Elijah <14240674+elijahcommits@users.noreply.github.com>
2022-11-25 22:38:37 -0800

    reset (center) image when mouse leaves container

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
-let constrain = 20;
 let mouseOverContainer = document.getElementById("header");
 let facingFaceImage = document.getElementById("my_avatar");
+
+// ==========================
+//  follow mouse (transform)
+// ==========================
+
+let constrain = 20;
 
 function transforms(x, y, el) {
   let box = el.getBoundingClientRect();
@@ -12,9 +17,31 @@ function transforms(x, y, el) {
     + "   rotateY("+ calcY +"deg) ";
 };
 
- function transformElement(el, xyEl) {
-  el.style.transform  = transforms.apply(null, xyEl);
+function transformElement(el, xyEl) {
+  el.style.transform = transforms.apply(null, xyEl);
 }
+
+// ============================
+//  to center (undo transform)
+// ============================
+
+function unTransforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(box.x / 2) / constrain;
+  let calcY = (box.y / 2) / constrain;
+  
+  return "perspective(none) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+function unTransformElement(el, xyEl) {
+  el.style.transform = unTransforms.apply(null, xyEl);
+}
+
+// ===============
+//  on mouse move
+// ===============
 
 mouseOverContainer.onmousemove = function(e) {
   let xy = [e.clientX, e.clientY];
@@ -22,5 +49,18 @@ mouseOverContainer.onmousemove = function(e) {
 
   window.requestAnimationFrame(function(){
     transformElement(facingFaceImage, position);
+  });
+};
+
+// ================
+//  on mouse leave
+// ================
+
+mouseOverContainer.onmouseleave = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([facingFaceImage]);
+
+  window.requestAnimationFrame(function(){
+    unTransformElement(facingFaceImage, position);
   });
 };
